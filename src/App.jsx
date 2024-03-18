06fee3b33a089f602635f0b5abd61710a9ff90a9
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-03-18 14:49:09 +0000

    First push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
