20e5600b525df342782d654b9c0b11e69585543d
callumhayden <callum.hayden@gmail.com>
2024-03-20 09:47:29 +0000

    fix: updated location on the map

--- src/components/contact/Map.jsx
+++ src/components/contact/Map.jsx
@@ -1,14 +1,12 @@
 export default function Map() {
-
-    return(
-        <>
-    <section className="py-16">
+  return (
+    <>
+      <section className="py-16">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-[#255036] mb-8">Our Location</h2>
           <div className="relative h-96 rounded-lg overflow-hidden">
-            {/* Replace the iframe src with your actual Google Maps embed URL */}
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6146847943637!2d-73.98731968458928!3d40.74844197932787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1623945318097!5m2!1sen!2sus"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19579.67731207821!2d0.3876931585938876!3d52.75405099871686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d7836894bd0cc7%3A0x61bdf4cbf84c5c2a!2sKing&#39;s%20Lynn%2C%20UK!5e0!3m2!1sen!2sus!4v1623945859204!5m2!1sen!2sus"
               width="100%"
               height="100%"
               style={{ border: 0 }}
@@ -18,6 +16,6 @@ export default function Map() {
           </div>
         </div>
       </section>
-        </>
-    )
+    </>
+  );
 }
