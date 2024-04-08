8a6dc83522708c363a98dfe8c3c0bd12d2e90747
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-04-08 14:13:01 +0100

    Title update and pricing page change

--- src/components/prices/PriceIntro.jsx
+++ src/components/prices/PriceIntro.jsx
@@ -4,11 +4,13 @@ export default function PriceIntro(){
         <div className="bg-[#255036] text-white py-12">
           <div className="max-w-4xl mx-auto text-center">
             <h1 className="text-4xl font-bold mb-8 text-[#ffcf40]">
-              Pricing Plans
+              Pricing That Fits Your Vision
             </h1>
             <p className="text-xl">
-              Choose the pricing plan that best fits your needs and budget. We
-              offer flexible options to suit various project requirements.
+              Bringing your vision to life shouldn't break the bank. Our
+              flexible pricing plans ensure that you can access high-quality web
+              design services without compromising on your budget. Explore our
+              options and discover the perfect fit for your project.
             </p>
           </div>
         </div>
